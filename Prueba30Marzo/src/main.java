0f67abba2665768dc60a95ad1b9dbe6bd7c81e80
Moises <Moises@192.168.0.10>
2020-04-02 17:28:35 -0600

    PRUEBA30Marzo

--- Prueba30Marzo/src/main.java
+++ Prueba30Marzo/src/main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,18 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String Nom, Dire;
+        int telefono;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese Nombre");
+            Nom = teclado.nextLine();
+        System.out.println("Ingrese Direccion");
+            Dire = teclado.nextLine();    
+        System.out.println("Ingrese Número de Telefono");
+            telefono = teclado.nextInt();    
+        System.out.println("Nombre..."+Nom);
+        System.out.println("Dirección..."+Dire);
+        System.out.println("Numero de Telefono..."+telefono);
     }
     
 }
