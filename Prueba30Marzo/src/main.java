12af7c877b5222266a5ea9d9657db1a648c3eb10
marvin502 <62572934+marvin502@users.noreply.github.com>
2020-04-02 18:46:27 -0600

    Update main.java

--- Prueba30Marzo/src/main.java
+++ Prueba30Marzo/src/main.java
@@ -42,6 +42,12 @@ public class main {
        System.out.println("Nombre: José Alejandro López Ramírez");
        System.out.println("Direccion: Huehuetenango Zona 8");
        System.out.println("Telefono: 55928636");
+       
+       System.out.println("Nombre: Marvin Arnoldo Ordoñez Jimenez");
+       System.out.println("Direccion: Huehuetenango Zona 8");
+       System.out.println("Telefono: 48548846");
+       
+       
     }
     
 }
